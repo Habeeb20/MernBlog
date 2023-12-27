29608c7b89f0b79ad8a127bba7bc7d1b90dfea4c
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2023-12-27 06:57:55 -0800

    editing

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const authController = require("./controllers/authController")
 const blogController = require("./controllers/blogController")
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 7000
+const port = process.env.PORT || 5000
 mongoose.set("strictQuery", false)
 mongoose
     .connect(process.env.MONGO)
@@ -21,8 +21,8 @@ mongoose
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
-app.use('/', authController)
-app.use('/', blogController)
+app.use('/auth', authController)
+app.use('/blog', blogController)
 
 
 app.listen(port,()=> {
---
+++ client/src/components/newsletter/Newsletter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './newsletter.module.css'
+const Newsletter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Newsletter
